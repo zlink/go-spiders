64d5dec9b670b718f3a5e1590389e817f5745591
zch <djyzzc@gmail.com>
2019-10-17 01:46:28 +0800

    update

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"spiders/defs"
 	"spiders/engine"
 )
 
 func main() {
-	var seeds []defs.Request
-	seeds = []defs.Request{
-		{
-			Url:   "http://www.zhenai.com/zhenghun/beijing",
-			Parse: engine.NewFuncParser(parser.ParseCity, "ParseCity"),
-		},
-	}
 	engine.Run()
 }
